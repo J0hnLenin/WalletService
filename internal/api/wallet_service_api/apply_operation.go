59330e6afef7f690995d90a27650082a57fcdf53
J0hnLenin <ziziz123ziziz@gmail.com>
2026-01-18 23:01:25 +0500

    написал методы api

--- internal/api/wallet_service_api/apply_operation.go
+++ internal/api/wallet_service_api/apply_operation.go
@@ -1,1 +1,33 @@
 package walletserviceapi
+
+import (
+	"context"
+
+	"github.com/J0hnLenin/WalletService/internal/models"
+	proto_models "github.com/J0hnLenin/WalletService/internal/pb/models"
+	"github.com/google/uuid"
+)
+
+func (api *WalletServiceAPI) ApplyOperation(ctx context.Context, op *proto_models.Operation) (*proto_models.Wallet, error) {
+	uuid := uuid.UUID([]byte(op.WalletId))
+
+	amount := op.Amount
+	if op.OperationType == proto_models.Operation_WITHDRAW {
+		amount = -amount
+	}
+
+	operation := &models.WalletOperation{
+		WalletID: uuid,
+		AmountChange: amount,
+	}
+	
+	wallet, err := api.walletService.ApplyOperation(ctx, operation)
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto_models.Wallet{
+		Id:      wallet.ID.String(),
+		Balance: wallet.Balance,
+	}, nil
+}
--- internal/api/wallet_service_api/get_balance.go
+++ internal/api/wallet_service_api/get_balance.go
@@ -1,1 +1,21 @@
 package walletserviceapi
+
+import (
+	"context"
+
+	proto_models "github.com/J0hnLenin/WalletService/internal/pb/models"
+	"github.com/J0hnLenin/WalletService/internal/pb/wallets_api"
+	"github.com/google/uuid"
+)
+
+func (api *WalletServiceAPI) GetBalance(ctx context.Context, req *wallets_api.GetBalanceRequest) (*proto_models.Wallet, error){
+	uuid := uuid.UUID([]byte(req.WalletId))
+	wallet, err := api.walletService.GetWalletByID(ctx, uuid)
+	if err != nil {
+		return nil, err
+	}
+	return &proto_models.Wallet{
+		Id: wallet.ID.String(),
+		Balance: wallet.Balance,
+	}, nil
+}
