b2463cc3b764f0638e5bfe3f17c5016045ef4aeb
J0hnLenin <ziziz123ziziz@gmail.com>
2026-01-18 17:38:47 +0500

    Дописал makefile

--- scripts/command.mk
+++ scripts/command.mk
@@ -1,6 +1,6 @@
 .PHONY: gen-api gen-moks
 gen-api:
-	powershell -ExecutionPolicy Bypass -File ./scripts/gen_api.ps1
+	powershell -ExecutionPolicy Bypass -File ./scripts/gen-api.ps1
 gen-moks:
 	powershell -ExecutionPolicy Bypass -File ./scripts/gen-moks.ps1
 
