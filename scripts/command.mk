4c29322b75a625e4d54e362374f2dbabc0950136
J0hnLenin <ziziz123ziziz@gmail.com>
2026-01-19 01:54:36 +0500

    Добавил команду запуска тестов и github workflow

--- scripts/command.mk
+++ scripts/command.mk
@@ -3,4 +3,6 @@ gen-api:
 	powershell -ExecutionPolicy Bypass -File ./scripts/gen-api.ps1
 gen-moks:
 	powershell -ExecutionPolicy Bypass -File ./scripts/gen-moks.ps1
+run-tests:
+	powershell -ExecutionPolicy Bypass -File ./scripts/run-tests.ps1
 
